b69aca7290b4787eac9f049a157b2996f9a3f01a
MaggieDaisy <missgosia84@gmail.com>
2021-08-24 11:38:39 +0000

    Product admin - Finished add product functionality

--- profiles/views.py
+++ profiles/views.py
@@ -8,23 +8,25 @@ from checkout.models import Order
 
 
 def profile(request):
-    """Display the users's profile"""
+    """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
-    if request.method == "POST":
+    if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
         'form': form,
         'orders': orders,
-        'on_profile_page': True,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
